56c67b48ff4b86eec70b929f9fa8a0f03f9cda7d
myadream <notice@myadream.com>
2022-05-17 03:10:32 +0800

    init

---
+++ src/main/java/com/myadream/app/api/ServiceApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.myadream.app.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceApiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApiSpringBootApplication.class, args);
+    }
+
+}
