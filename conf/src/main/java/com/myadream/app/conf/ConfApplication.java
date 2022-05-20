46b2f618d3462b50c7561a2e5c2675987e3a3696
myaream <notice@myadream.com>
2022-05-21 00:35:42 +0800

    modify

---
+++ conf/src/main/java/com/myadream/app/conf/ConfApplication.java
@@ -0,0 +1,10 @@
+package com.myadream.app.conf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class ConfApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfApplication.class, args);
+    }
+}
